204e50903ebb4d952d0724db030d8e3c29fd7152
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-06-28 09:51:54 +0200

    Fixed null in json.

--- src/board/metaviz-client-ic/editor/editor.js
+++ src/board/metaviz-client-ic/editor/editor.js
@@ -44,11 +44,13 @@ class MetavizEditorIC extends MetavizEditorBrowser {
                 this.category = doc.data.category;
                 this.version = doc.version;
 
-                // If error - show alert
-                if ('error' in doc.data.json) alert(doc.data.json.error);
-
-                // Decode data
-                metaviz.format.deserialize('text/metaviz+json', doc.data.json);
+                // Deserialize json                
+                if (doc.data.json) {
+                    // If error - show alert
+                    if ('error' in doc.data.json) alert(doc.data.json.error);
+                    // Decode data
+                    else metaviz.format.deserialize('text/metaviz+json', doc.data.json);
+                }
 
                 // Ready
                 this.idle();
