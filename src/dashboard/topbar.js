2d76045df2e144fb57a1db79d327b6181917db04
Dariusz Dawidowski <dariusz.dawidowski@gmail.com>
2024-08-12 20:32:49 +0200

    Fetching Metaviz version. Testing info.

--- src/dashboard/topbar.js
+++ src/dashboard/topbar.js
@@ -5,5 +5,8 @@ export const renderTopbar = (args) => {
         <div class="topbar">
             <div>Your resources in ${args.boards} boards and ${args.categories} categories</div>
         </div>
+        <div class="topbar" style="background-color: #eee; color: #000;">
+            <div><span class="mdi mdi-alert" style="font-size: 1.2em;"></span> This version is for testing and demonstration purposes only. Please do not store any important data here before opening the formal application.</div>
+        </div>
     `;
 };
